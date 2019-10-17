b7126999aae53256d4d85881ceecbf88edf74189
alexlo1 <alexdlo8967@gmail.com>
2019-10-17 13:04:35 -0700

    Updates

---
+++ precache-manifest.8e3e564b587abb8067cb399cbe63833e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e711d9510df32a3efba0567c1e53bb91",
+    "url": "/Minesweeper/index.html"
+  },
+  {
+    "revision": "0667e5f2a6b92d88cde3",
+    "url": "/Minesweeper/static/css/main.19ad4b97.chunk.css"
+  },
+  {
+    "revision": "d21d1401bc1fd9aa653d",
+    "url": "/Minesweeper/static/js/2.c7b9905d.chunk.js"
+  },
+  {
+    "revision": "0667e5f2a6b92d88cde3",
+    "url": "/Minesweeper/static/js/main.c1787807.chunk.js"
+  },
+  {
+    "revision": "4b2180b374d2925f4fc4",
+    "url": "/Minesweeper/static/js/runtime-main.5daf0edb.js"
+  },
+  {
+    "revision": "03633cd723f80ce88b7d92697336817d",
+    "url": "/Minesweeper/static/media/bomb.03633cd7.svg"
+  },
+  {
+    "revision": "8f20765f8daf1345641d125bd3616aa1",
+    "url": "/Minesweeper/static/media/flag.8f20765f.svg"
+  }
+]);
